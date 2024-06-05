49a7ed55a60bd6a5ff94105ce91fa11239f3344b
tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-06-05 19:53:43 +0200

    zmiany Ada

--- Ada/dining_philosophers.adb
+++ Ada/dining_philosophers.adb
@@ -1,84 +1,94 @@
-with Ada.Text_IO;
-with Ada.Integer_Text_IO;
-with Ada.Calendar;
-with Ada.Real_Time;
+with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Numerics.Discrete_Random;
-use Ada.Text_IO;
-use Ada.Integer_Text_IO;
-use Ada.Calendar;
-use Ada.Real_Time;
+with Ada.Synchronous_Task_Control; use Ada.Synchronous_Task_Control;
+with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Dining_Philosophers is
-   NUM_PHILOSOPHERS : constant Positive := 5;
-   TASK_DURATION_MIN : constant Time_Span := Milliseconds (100);
-   TASK_DURATION_MAX : constant Time_Span := Milliseconds (1000);
 
-   package Rand_Gen is new Ada.Numerics.Discrete_Random (1 .. 1000);
-   use Rand_Gen;
-   Gen : Generator;
+   Number_Of_Philosophers : constant Integer := 5;
 
-   type Fork is tagged null record;
-   type Fork_Access is access all Fork;
-   Forks : array (1 .. NUM_PHILOSOPHERS) of Fork_Access;
+   protected type Fork is
+      procedure Pick_Up;
+      procedure Put_Down;
+   private
+      Is_Available : Boolean := True;
+   end Fork;
 
-   task type Philosopher (Id : Positive);
-   task body Philosopher is
-      procedure Random_Sleep (Duration_Min, Duration_Max : Time_Span) is
-         Start : Time := Clock;
-         Duration : Time_Span := Duration_Min + Time_Span'Fraction(Float(Sample(Gen)) / 1000.0) * (Duration_Max - Duration_Min);
+   protected body Fork is
+      procedure Pick_Up is
       begin
-         delay Duration;
-      end Random_Sleep;
+         while not Is_Available loop
+            delay 0.001;
+         end loop;
+         Is_Available := False;
+      end Pick_Up;
+
+      procedure Put_Down is
+      begin
+         Is_Available := True;
+      end Put_Down;
+   end Fork;
+
+   type Philosopher_Index is range 1 .. Number_Of_Philosophers;
+
+   type Philosopher_Array is array (Philosopher_Index) of Fork;
+   Forks : Philosopher_Array;
+
+   task type Philosopher (ID : Philosopher_Index) is
+      entry Start;
+   end Philosopher;
+
+   task body Philosopher is
+      Left_Fork, Right_Fork : Fork renames Forks (ID), Forks ((ID mod Number_Of_Philosophers) + 1);
+      package Random_Delay is new Ada.Numerics.Discrete_Random (Duration);
+      Gen : Random_Delay.Generator;
    begin
-      for I in 1 .. 3 loop
-         -- Thinking
-         Put_Line("Philosopher " & Positive'Image(Id) & " is thinking");
-         Random_Sleep(TASK_DURATION_MIN, TASK_DURATION_MAX);
+      accept Start;
 
-         -- Eating
-         if Id = 1 then
-            Forks(Id).all.Lock;
-            Forks(NUM_PHILOSOPHERS).all.Lock;
-         else
-            Forks(Id).all.Lock;
-            Forks(Id - 1).all.Lock;
-         end if;
-         Put_Line("Philosopher " & Positive'Image(Id) & " is eating");
-         Random_Sleep(TASK_DURATION_MIN, TASK_DURATION_MAX);
-
-         -- Finished eating
-         if Id = 1 then
-            Forks(NUM_PHILOSOPHERS).all.Unlock;
-            Forks(Id).all.Unlock;
-         else
-            Forks(Id - 1).all.Unlock;
-            Forks(Id).all.Unlock;
-         end if;
-         Put_Line("Philosopher " & Positive'Image(Id) & " finished eating");
+      loop
+         Think;
+         Left_Fork.Pick_Up;
+         Right_Fork.Pick_Up;
+         Eat;
+         Right_Fork.Put_Down;
+         Left_Fork.Put_Down;
       end loop;
    end Philosopher;
 
-   task type Fork is
-      entry Lock;
-      entry Unlock;
-   end Fork;
+   Philosophers : array (Philosopher_Index) of Philosopher;
 
-   task body Fork is
+   procedure Think (ID : Philosopher_Index) is
+      package Random_Delay is new Ada.Numerics.Discrete_Random (Duration);
+      Gen : Random_Delay.Generator;
+      Delay_Time : Duration;
    begin
-      loop
-         accept Lock;
-         accept Unlock;
-      end loop;
-   end Fork;
+      Put_Line ("Philosopher " & ID'Img & " is thinking");
+      Random_Delay.Reset (Gen);
+      Delay_Time := Random_Delay.Random (Gen) * 1.0;
+      delay Delay_Time;
+   end Think;
 
-   Philosopher_Tasks : array (1 .. NUM_PHILOSOPHERS) of Philosopher := (others => Philosopher'(Id => <>));
+   procedure Eat (ID : Philosopher_Index) is
+      package Random_Delay is new Ada.Numerics.Discrete_Random (Duration);
+      Gen : Random_Delay.Generator;
+      Delay_Time : Duration;
+   begin
+      Put_Line ("Philosopher " & ID'Img & " is eating");
+      Random_Delay.Reset (Gen);
+      Delay_Time := Random_Delay.Random (Gen) * 1.0;
+      delay Delay_Time;
+      Put_Line ("Philosopher " & ID'Img & " finished eating");
+   end Eat;
 
 begin
-   -- Initialize forks
-   for I in Forks'Range loop
-      Forks(I) := new Fork;
+   for I in Philosopher_Index loop
+      Philosophers (I).Start;
    end loop;
 
-   -- Wait for all philosophers to finish
-   delay 10.0;
+   delay 20.0;
+
+   for I in Philosopher_Index loop
+      abort Philosophers (I);
+   end loop;
 end Dining_Philosophers;
+
